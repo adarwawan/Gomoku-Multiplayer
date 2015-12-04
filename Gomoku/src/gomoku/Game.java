890be3b12f428859239bbc17c78c0251f2604ea3
Ahmad Darmawan <adarwawan@gmail.com>
2015-12-04 08:52:04 +0700

    Logic in Server

--- Gomoku/src/gomoku/Game.java
+++ Gomoku/src/gomoku/Game.java
@@ -50,7 +50,6 @@ public class Game {
             if (checkwin) {
                 break;
             }
-        }
-    
+        }    
     }
 }
---
+++ Gomoku/src/servergomoku/Room.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servergomoku;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author adar
+ */
+public class Room {
+    /* data */
+    private int idRoom;
+    private String roomname;
+    private int playerMin;
+    int AbsisOrdinatMove = 2;
+    private ArrayList<User> listPlayer;
+    private int playerAvailable;
+    private int idTurn; // Lagi jalan siapa
+    private int status; // Waiting = 0; Play = 1; End = 2;
+    private Board board;
+    private int idWin; // Initiate = 0;
+    
+    /* method */
+    public Room (int _idRoom, String _roomname, int _playerMin, User _user) {
+        idRoom = _idRoom;
+        roomname = _roomname;
+        playerMin = _playerMin;
+        listPlayer = new ArrayList<>();
+        listPlayer.add(_user);
+        playerAvailable = 1;
+    }
+    
+    public void addPlayer(User _user) {
+        listPlayer.add(_user);
+        playerAvailable++;
+    }
+    
+    public void startGame () {
+        board = new Board();
+        board.setNumPlayer(playerMin);//set jumlah player
+        status = 1;
+        idTurn = 1;
+    }
+    
+    public void userTurn (int[] move, int _idUserInRoom) {
+        int validMove = 0;
+        validMove = board.makeMove((_idUserInRoom), move[0], move[1]);
+        int win = board.checkWin();
+        if (win == (_idUserInRoom)) {
+            status = 2;
+            idWin = _idUserInRoom;
+        }
+    }
+    
+    public void pesanKemenangan (int _idUserInRoom) {
+        if (idWin == _idUserInRoom) {
+            System.out.println("Selamat Anda Menang!");
+        }
+        else {
+            System.out.println("Pemenangnya adalah user ke- " + _idUserInRoom);
+        }
+    }
+    
+    public int getIdRoom () {
+        return idRoom;
+    }
+    
+    public String getRoomname () {
+        return roomname;
+    }
+    
+    public int getPlayerMin () {
+        return playerMin;
+    }
+    
+    public int getPlayerAvailable () {
+        return playerAvailable;
+    }
+    
+    public int getIdTurn () {
+        return idTurn;
+    }
+    
+    public int getStatus () {
+        return status;
+    }
+    
+    public void setIdRoom (int _idRoom) {
+        idRoom = _idRoom;
+    }
+    
+    public void setRoomname (String _roomname) {
+        roomname = _roomname;
+    }
+    
+    public void setPlayerMin (int _playerMin) {
+        playerMin = _playerMin;
+    }
+    
+    public void setPlayerAvailable (int _playerAvailable) {
+        playerAvailable = _playerAvailable;
+    }
+    
+    public void setIdTurn (int _idTurn) {
+        idTurn = _idTurn;
+    }
+    
+    public void setStatus (int _status) {
+        status = _status;
+    }
+    
+}
